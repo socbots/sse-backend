56eb17ee0bb4b1fd05b98db0b95b35529ae06d1f
Johan Penttinen <johanlepenttinen@gmail.com>
2021-04-19 14:59:37 +0300

    adds bodyPart property that mobilesdk wants

--- testing/move.py
+++ testing/move.py
@@ -3,6 +3,8 @@ import requests
 
 while True:
     input("POST a move... Press enter")
-    res = requests.post('http://localhost:5000/move', json={"gesture":"wave"})
+    res = requests.post(
+        "http://localhost:5000/move", json={"gesture": "wave", "bodyPart": "left_hand"}
+    )
     if res.ok:
         print(res.json())

a0a5937ededa7d07dff5af8bda8f0ca79edf392f
Johan Penttinen <johanlepenttinen@gmail.com>
2021-05-25 13:28:40 +0300

    Update move.py

--- testing/move.py
+++ testing/move.py
@@ -1,10 +1,25 @@
 import time
 import requests
 
+
+
 while True:
     input("POST a move... Press enter")
+    
+    """ res = requests.post(
+        "http://localhost:5000/move", json={
+            "gesture": "wave", 
+            "bodyPart": "right_hand"
+            }
+    )
+     """
+
     res = requests.post(
-        "http://localhost:5000/move", json={"gesture": "wave", "bodyPart": "left_hand"}
+        "http://localhost:5000/move", json={
+            "direction": "up", 
+            "bodyPart": "head",
+            "distance": 3
+            }
     )
     if res.ok:
         print(res.json())
